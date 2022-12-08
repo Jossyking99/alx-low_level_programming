8109e6b460244a483365b1252099bcdd02aa1b29
Jossyking99 <Josephakharume97@gmail.com>
2022-12-08 00:59:19 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
