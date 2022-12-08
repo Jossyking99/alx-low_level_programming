ada099718096bac4825a68c7edf309de17c7cf52
Jossyking99 <Josephakharume97@gmail.com>
2022-12-08 03:41:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
+
+
 /**
- * main - program entry point
- * Return -  Always return 1
- */
+ *
+ *  *main - Entry point
+ *
+ *   *Return: Always 1
+ *
+ *    *
+ *
+ *     */
+
+
 
 int main(void)
+
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+
 }
