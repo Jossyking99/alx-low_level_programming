43ef476fcdea92fdd7ea1c8c0cf65f73fa5a2a49
Jossyking99 <Josephakharume97@gmail.com>
2022-12-10 12:32:34 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry of the program
- *
  * Return: Always returns (0)
  */
 
 int main(void)
-{ 
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0);
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,7 @@ int main (void)
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(init));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n". sizeof(long long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 return (0);
 }
