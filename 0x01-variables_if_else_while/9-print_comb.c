6765c09635f0767a6090fd99ab94c4f49aab646c
Jossyking99 <Josephakharume97@gmail.com>
2022-12-10 05:07:32 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+	int a;
+
+	for (a = 0; a < 10; a++)
+       	{
+		putchar(a + '0');
+		if (a < 9)
+	       	{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+	}
