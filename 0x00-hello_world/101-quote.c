160534e5b0f55dd2b61b0afeaa1c64456107e0fe
Jossyking99 <Josephakharume97@gmail.com>
2022-12-10 21:16:58 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
-
-
 /**
- *  main -program entry point
- *  Return: Always return 1    
- */
-
-
-
+*main - Entry point
+*Return: 1
+*/
 int main(void)
-
 {
-
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
