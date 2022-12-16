08cb3371de5de489aadcbdbac908d8745eb457c7
Jossyking99 <Josephakharume97@gmail.com>
2022-12-16 11:33:05 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if(i != 98)
+			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			if(j != 98)
+			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
 				printf("%d\n", j);
