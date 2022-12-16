e28ee1f305e506797cef54c1c2004d1524968cdb
Jossyking99 <Josephakharume97@gmail.com>
2022-12-15 22:28:07 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,12 +13,14 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			for (j = 0; j <= n; j++)
+			{
 			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
 			} else if (k < 10 && j != 0)
-			}
+			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
@@ -37,4 +39,10 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar((k / 100) + '0');
 				_putchar(((k / 10) % 10) + '0');
-
+				_putchar((k % 10) + '0');
+			}
+		}
+			_putchar('\n');
+		}
+	}
+}
