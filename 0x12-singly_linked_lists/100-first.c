c7b9a49b6c360369d05bbda38ed8176a7a18ba31
ypnconnect <ypnconnect@gmail.com>
2023-01-27 13:04:34 -0800

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,7 @@ void first(void) __attribute__ ((constructor));
  */
 void first(void)
 {
- printf("You're beat! and yet, you must allow,\n");
- printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
 
