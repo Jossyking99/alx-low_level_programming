10ce67ef7ac66e4d760da6ef95201061bc890cef
ypnconnect <ypnconnect@gmail.com>
2023-01-30 13:48:24 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,22 +1,49 @@
 #include "lists.h"
 
 /**
- * sum_listint - Calculates the sum of all the
- *               data (n) of a listint_t list.
- * @head: A pointer to the head of the listint_t list.
- *
- * Return: If the list is empty - 0.
- *         Otherwise - the sum of all the data.
+ * insert_nodeint_at_index - function that insert a node in a idx position
+ * @head: pointer to head in function
+ * @idx: index or position to be added
+ * @n: node's value
+ * Return: nth node data
  */
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int sum = 0;
+	unsigned int nodes;
+	listint_t *node_index = *head;
+	listint_t *new_node, *node_after;
 
-	while (head)
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	for (nodes = 0; node_index != NULL; nodes++)
+		node_index = node_index->next;
+
+	if (idx > (nodes + 1))
+		return (NULL);
+
+	node_index = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	if (idx == 0)
 	{
-		sum += head->n;
-		head = head->next;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-
-	return (sum);
+	else
+	{
+		node_after = *head;
+		for (nodes = 0; nodes < (idx - 1); nodes++)
+			node_index = node_index->next;
+		for (nodes = 0; nodes < idx; nodes++)
+			node_after = node_after->next;
+		node_index->next = new_node;
+		new_node->next = node_after;
+		return (new_node);
+	}
 }
