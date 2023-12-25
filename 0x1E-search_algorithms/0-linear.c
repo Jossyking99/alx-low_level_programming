3a07e1f4cfbe014193d1e22571435c7c4bd223ac
Jossyking99 <Josephakharume97@gmail.com>
2023-12-25 10:46:51 +0000

    files update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of
+ * linear_search - This searches for a value in an array of
  * integers using the Linear search algorithm
  *
  * @array: input array
